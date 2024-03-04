e3851e344e5763fe0b7d99624c768a9a31d4480a
sofia <fernandesofia1802@gmail.com>
2024-03-04 21:50:47 +0100

    se cargan archivos html, en assets

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,26 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 
 module.exports = (_, argv) => ({
-    entry: './src/index.js',
+    entry: './src/index.js', // Archivo de entrada
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].bundle.js', // Archivo de salida
         path: path.resolve(__dirname, 'dist'),
     },
-    experiments: {
+    experiments: { // Habilitar el uso de módulos ESM
         outputModule: true,
     },
-    devServer: {
+    devServer: { // Configuración del servidor de desarrollo
         port: 3000,
         historyApiFallback: true,
     },
-    devtool: argv.mode === 'development' ? 'source-map' : 'false',
+    devtool: argv.mode === 'development' ? 'source-map' : 'false', // Generar source maps
     module: {
         rules: [
             {
-                test: /\.m?js$/,
+                test: /\.m?js$/, // Cargar archivos JS y JSX
                 exclude: /node_modules/,
                 type: 'javascript/auto',
                 resolve: {
@@ -28,7 +30,7 @@ module.exports = (_, argv) => ({
                 },
             },
             {
-                test: /\.(ts|tsx|js|mjs|jsx)$/,
+                test: /\.(ts|tsx|js|mjs|jsx)$/, // Cargar archivos JS y JSX
                 exclude: /node_modules/,
                 use: ['babel-loader'],
             },
@@ -41,6 +43,20 @@ module.exports = (_, argv) => ({
                     'sass-loader',
                 ]
             },
+            { // Cargar imágenes    
+                test: /\.(png|jpg|jpeg|gif|svg|ico|ttf|woff|woff2)$/i,
+                type: 'asset/resource', // Cargar archivos como recursos
+                exclude: /node_modules/,
+                generator: {
+                    filename: 'assets/[name][ext]' // Carpeta de salida
+                }
+            },
+            {
+                test: /\.html$/i, // Cargar archivos HTML
+                loader: 'html-loader',
+                exclude:/node_modules/,
+
+            }
             
         ],
     },
@@ -52,5 +68,10 @@ module.exports = (_, argv) => ({
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
+        new CopyWebpackPlugin({ // Copiar archivos de la carpeta src/img a dist/assets
+            patterns: [
+                { from: './src/img', to: 'assets' } // Copiar archivos de la carpeta src/assets a dist/assets
+            ]
+        })
     ],
 });
